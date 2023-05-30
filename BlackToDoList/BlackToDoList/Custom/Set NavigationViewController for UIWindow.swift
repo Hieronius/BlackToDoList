1b611bd5b4880b01405ab0448c23106d46245df3
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-30 10:24:51 +0300

    Add method to set NavigationStack for UIWindow

---
+++ BlackToDoList/BlackToDoList/Custom/Set NavigationViewController for UIWindow.swift
@@ -0,0 +1,30 @@
+//
+//  Set NavigationViewController for UIWindow.swift
+//  BlackToDoList
+//
+//  Created by Арсентий Халимовский on 30.05.2023.
+//
+
+import Foundation
+
+// Remove this file fast as possible
+
+//Method to set windowRootViewController as NagivationViewController with rootStoryboard(ViewController)
+//After you can just change root view controller of this NaviVC and do a lot of stuff later
+
+
+//var window: UIWindow?
+//
+//func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//
+//guard let windowScene = (scene as? UIWindowScene) else { return }
+//
+//        let storyboard = UIStoryboard(name: "Welcome", bundle: nil)
+//        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+//        let rootViewController = storyboard.instantiateViewController(withIdentifier: "WelcomeViewController") as! WelcomeViewController
+//        navigationController.viewControllers = [rootViewController]
+//        let window = UIWindow(windowScene: windowScene)
+//        window.rootViewController = navigationController
+//        self.window = window
+//        window.makeKeyAndVisible()
+//        }
