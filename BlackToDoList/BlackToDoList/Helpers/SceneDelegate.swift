1bc0c3eb9ab700727d78c5aa2c53b36f1816ccfa
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-31 17:52:14 +0300

    Create storyboards for Reset/Registration

--- BlackToDoList/BlackToDoList/Helpers/SceneDelegate.swift
+++ BlackToDoList/BlackToDoList/Helpers/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         /// Define NavigationViewController  steck with LoginViewController as it's root view controller
-        let storyboard = UIStoryboard(name: "LoginViewController", bundle: nil)
+        let storyboard = UIStoryboard(name: "LogInViewController", bundle: nil)
         let navigationViewController = storyboard.instantiateInitialViewController() as! UINavigationController
         let rootViewController = storyboard.instantiateViewController(withIdentifier: "LogInViewController") as! LogInViewController
         navigationViewController.viewControllers = [rootViewController]
@@ -29,33 +29,43 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.makeKeyAndVisible()
         
     }
+    
+//    var window: UIWindow?
+//
+//
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//
+//
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//
+//        let storyboard = UIStoryboard(name: "Welcome", bundle: nil)
+//        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+//        let rootViewController = storyboard.instantiateViewController(withIdentifier: "WelcomeViewController") as! WelcomeViewController
+//        navigationController.viewControllers = [rootViewController]
+//        let window = UIWindow(windowScene: windowScene)
+//        window.rootViewController = navigationController
+//        self.window = window
+//        window.makeKeyAndVisible()
+//    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        
     }
 
 
