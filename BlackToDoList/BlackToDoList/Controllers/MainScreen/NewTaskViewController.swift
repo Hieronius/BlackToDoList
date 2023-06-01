34d86588e5b45900710cf192b956d43e6386b62c
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-01 06:05:30 +0300

    Add NewTaskViewController

---
+++ BlackToDoList/BlackToDoList/Controllers/MainScreen/NewTaskViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewTaskViewController.swift
+//  BlackToDoList
+//
+//  Created by Арсентий Халимовский on 01.06.2023.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
