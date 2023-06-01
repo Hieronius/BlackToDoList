2d961a9b021c2ecf0b0a486987e3a393408c223a
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-01 06:03:00 +0300

    Create NaviVC and define MainScreenVC

--- BlackToDoList/BlackToDoList/Controllers/MainScreen/MainScreenViewController.swift
+++ BlackToDoList/BlackToDoList/Controllers/MainScreen/MainScreenViewController.swift
@@ -5,4 +5,13 @@
 //  Created by Арсентий Халимовский on 30.05.2023.
 //
 
-import Foundation
+import UIKit
+
+class MainScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
