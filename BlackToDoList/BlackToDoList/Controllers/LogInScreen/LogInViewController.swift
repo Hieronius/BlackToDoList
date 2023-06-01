28b80a10e2ae0df13e2edc19a9022120b66cad78
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-01 05:55:15 +0300

    Add outlets for my LogIn elements

--- BlackToDoList/BlackToDoList/Controllers/LogInScreen/LogInViewController.swift
+++ BlackToDoList/BlackToDoList/Controllers/LogInScreen/LogInViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var LogInEmailTextField: UITextField!
+    @IBOutlet private weak var LogInPasswordTextField: UITextField!
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - IBActions
+    
+    @IBAction func LogInButtonAction(_ sender: UIButton) {
+    }
+    
 
 }
 
