2d6a96d74b6bd9b7b268a51801497b3b68f32ed0
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-12 09:06:39 +0300

    Add KeychainManager

---
+++ BlackToDoList/BlackToDoList/Managers/KeychainManager.swift
@@ -0,0 +1,21 @@
+//
+//  KeyChainManager.swift
+//  BlackToDoList
+//
+//  Created by Арсентий Халимовский on 12.06.2023.
+//
+
+import UIKit
+
+final class KeychainManager {
+    
+    // Get data and encrypt it
+    static func save() {
+        // service, account, password, class, data
+    }
+    
+    // Find data and give it
+    static func get() {
+        // service, account, class, data, matchlimit
+    }
+}
