adcc12d83bf47c3645a1e6c1f5fdfaa938f0e503
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-12 09:08:19 +0300

    Add Enum to handle errors

--- BlackToDoList/BlackToDoList/Managers/KeychainManager.swift
+++ BlackToDoList/BlackToDoList/Managers/KeychainManager.swift
@@ -9,13 +9,19 @@ import UIKit
 
 final class KeychainManager {
     
-    // Get data and encrypt it
-    static func save() {
-        // service, account, password, class, data
+    // Enum for handling errors of the Keychain.
+    enum KeychainError: Error {
+        case duplicateEntry
+        case unknown(OSStatus)
     }
     
-    // Find data and give it
+    // Get data and encrypt it.
+    static func save() {
+        // service, account, password, class, data.
+    }
+    
+    // Find data and give it.
     static func get() {
-        // service, account, class, data, matchlimit
+        // service, account, class, data, matchlimit.
     }
 }
