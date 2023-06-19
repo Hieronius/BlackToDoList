0a5ccb7f87130aee166d5505f8f90bde4310a9a6
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-19 09:32:02 +0300

    Add userSessionManager

---
+++ BlackToDoList/BlackToDoList/Managers/UserSessionManager.swift
@@ -0,0 +1,13 @@
+//
+//  UserSessionManager.swift
+//  BlackToDoList
+//
+//  Created by Арсентий Халимовский on 19.06.2023.
+//
+
+import Foundation
+
+final class UserSessionManager {
+    // Current status of the user session.
+    static var isUserLoggedIn = false
+}
