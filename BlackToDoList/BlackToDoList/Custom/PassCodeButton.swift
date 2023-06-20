c101f61c29571000e8be1fa8fd1f78305e649f9b
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-20 21:05:50 +0300

    Add custom animation for passcode buttons

--- BlackToDoList/BlackToDoList/Custom/PassCodeButton.swift
+++ BlackToDoList/BlackToDoList/Custom/PassCodeButton.swift
@@ -8,7 +8,7 @@
 import UIKit
 /// Custom class for pass code numbers buttons to press.
 class PassCodeButton: UIButton {
-     // Change it's appearance when the user can see the screen.
+    // Change it's appearance when the user can see the screen.
     override func awakeFromNib() {
         super.awakeFromNib()
         // Define buttons as a circle.
@@ -19,18 +19,52 @@ class PassCodeButton: UIButton {
     
     // Add Animation to the button.
     @objc private func buttonClickedAnimation(sender: UIButton) {
+        
+        // Button fades out.
         DispatchQueue.main.async {
-            sender.alpha = 0.5
             sender.backgroundColor = .white
+            sender.alpha = 0.9
         }
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            sender.backgroundColor = .white
+            sender.alpha = 0.8
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.06) {
+            sender.backgroundColor = .white
+            sender.alpha = 0.7
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.07){
+            sender.backgroundColor = .white
+            sender.alpha = 0.6
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.08) {
+            sender.backgroundColor = .white
+            sender.alpha = 0.5
+        }
+        
+        // Button light up.
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             sender.backgroundColor = .black
             sender.alpha = 0.6
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.11) {
             sender.alpha = 0.7
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.12) {
             sender.alpha = 0.8
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.13) {
             sender.alpha = 0.9
-            sender.alpha = 1.0
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.14) {
+            sender.alpha = 1
         }
     }
 }
