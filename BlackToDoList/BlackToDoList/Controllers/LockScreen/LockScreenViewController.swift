6037ed0fd190866b5bb1172d1f025fb5f350df7b
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-23 09:18:56 +0300

    Clean some code

--- BlackToDoList/BlackToDoList/Controllers/LockScreen/LockScreenViewController.swift
+++ BlackToDoList/BlackToDoList/Controllers/LockScreen/LockScreenViewController.swift
@@ -240,12 +240,12 @@ final class LockScreenViewController: UIViewController {
     }
     
     // MARK: LOGOUT FROM THE APP
-        /// This function is an IBAction function that is triggered when the user taps the "Log Out" button in the app. It shows an alert to confirm if the user wants to log out, and if the user confirms, it logs the user out of the app, sets the "isUserLoggedIn" flag to false, and segues to the login screen
-        ///
-        /// - Parameters:
-        ///     - sender: An object that represents the sender of the action.
-        ///
-        /// - Returns: This function does not return any value.
+    /// This function is an IBAction function that is triggered when the user taps the "Log Out" button in the app. It shows an alert to confirm if the user wants to log out, and if the user confirms, it logs the user out of the app, sets the "isUserLoggedIn" flag to false, and segues to the login screen
+    ///
+    /// - Parameters:
+    ///     - sender: An object that represents the sender of the action.
+    ///
+    /// - Returns: This function does not return any value.
     @IBAction func logOutButtonAction(_ sender: Any) {
         showAlert(title: "LogOut", message: "Are you sure to logout from the app?", isCancelButton: true, okButtonName: "Relogin") {
             AuthManager.logOut()
--- BlackToDoList/BlackToDoList/Managers/AuthManager.swift
+++ BlackToDoList/BlackToDoList/Managers/AuthManager.swift
@@ -25,35 +25,24 @@ final class AuthManager {
     static func logOut() {
         let firebaseAuth = FirebaseAuth.Auth.auth()
         
-        // Check is there a real user in the syster with do-try-catch code.
         do {
             try firebaseAuth.signOut()
-            print("Logout was completed successfully")
-            // Don't know why we use NSError instead of just an Error.
         } catch let signOutError {
             print("Error signing out: %@", signOutError)
         }
     }
     
     static func resetPassword(_ email: String?, _ viewController: UIViewController) {
-        
-        
-            // Check is our email text field empty.
             let email = email ?? ""
             
-            // If email text field is empty throw an alert.
             if email.isEmpty {
                 viewController.showAlert(title: "Empty field", message: "Email text field is empty")
                 
-                // If email is not valid throw an alert.
             } else if !viewController.isValidEmail(email) {
                 viewController.showAlert(title: "Not valid email", message: "Please check and correct your email")
                 
             } else {
                 FirebaseAuth.Auth.auth().sendPasswordReset(withEmail: email)
-                print("Email with a link to change your password has been send")
-                // MARK: ALERT CONTROLLER - "LINK HAS BEEN SENT"
-                // There should be redirection to the LogInScreen.
                 viewController.showAlert(title: "Done",
                           message: "Please check your email address to create a new password)") {
                     viewController.navigationController?.popViewController(animated: true)
