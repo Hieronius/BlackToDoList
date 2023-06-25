8b5b2200fbbe75a408e329eb0af57156acc7db17
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-25 09:23:30 +0300

    Create a custom segue method
    With redirection to the main screen from the current screen

---
+++ BlackToDoList/BlackToDoList/Extensions/UIViewController+Extension (segue).swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Extension (segue).swift
+//  BlackToDoList
+//
+//  Created by Арсентий Халимовский on 24.06.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func segueToMainScreenAndMakeItAsRootTest(_ currentViewController: UIViewController) {
+        Task {
+            let storyboard = UIStoryboard(name: "MainScreenViewController", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "MainScreenViewController") as! MainScreenViewController
+            currentViewController.navigationController?.setViewControllers([viewController], animated: true)
+        }
+    }
+}
