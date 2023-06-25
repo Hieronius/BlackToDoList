8cbeb0e930323ced6c2f9d82c3cfee78dc8dae77
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-25 10:54:14 +0300

    Add documentation for NWInterface extension

--- BlackToDoList/BlackToDoList/Extensions/NWInterface+Extension.swift
+++ BlackToDoList/BlackToDoList/Extensions/NWInterface+Extension.swift
@@ -9,7 +9,9 @@ import Foundation
 import UIKit
 import Network
 
+
 extension NWInterface.InterfaceType: CaseIterable {
+    /// An array of all possible cases of the `NWInterface.InterfaceType` enumeration, including `.other`, `.wifi`, `.cellular`, `.loopback`, and `.wiredEthernet`. This property is required by the `CaseIterable` protocol.
     public static var allCases: [NWInterface.InterfaceType] = [
             .other,
             .wifi,
