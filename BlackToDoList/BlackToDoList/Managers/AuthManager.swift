0268876a1e56d5c8e4f3b53ffc5a1e7170345d03
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-27 09:07:37 +0300

    Refactor AuthManager documetation

--- BlackToDoList/BlackToDoList/Managers/AuthManager.swift
+++ BlackToDoList/BlackToDoList/Managers/AuthManager.swift
@@ -10,13 +10,15 @@ import UIKit
 
 final class AuthManager {
 
-    /// function is a static function that is used to sign up a new user with their email and password. This function takes four parameters: user's email address, user's password, user's password verification and the view controller that is calling the function. If user's email and password are correct this function should create a new account and send a verification link to the user's email address and redirect user to the LogIn Screen. Otherwice, it's shows an alert with an error message.
-    /// 
-    /// - Parameters:
-    ///   - userEmail: A String that represents the user's email address. This parameter is optional and defaults to an empty string if it is not provided.
-    ///   - userPassword: A String that represents the user's password. This parameter is optional and defaults to an empty string if it is not provided.
-    ///   - repeatPassword: A String that represents the user's repeated password. This parameter is optional and defaults to an empty string if it is not provided.
-    ///   - currentViewController: A UIViewController that represents the current view controller. This parameter is required and must be provided when calling the function.
+    /**
+    function is a static function that is used to sign up a new user with their email and password. This function takes four parameters: user's email address, user's password, user's password verification and the view controller that is calling the function. If user's email and password are correct this function should create a new account and send a verification link to the user's email address and redirect user to the LogIn Screen. Otherwice, it's shows an alert with an error message.
+    
+    - Parameters:
+       - userEmail: A String that represents the user's email address. This parameter is optional and defaults to an empty string if it is not provided.
+       - userPassword: A String that represents the user's password. This parameter is optional and defaults to an empty string if it is not provided.
+       - repeatPassword: A String that represents the user's repeated password. This parameter is optional and defaults to an empty string if it is not provided.
+       - currentViewController: A UIViewController that represents the current view controller. This parameter is required and must be provided when calling the function.
+     */
     static func signUp(_ userEmail: String?,
                        _ userPassword: String?,
                        _ repeatPassword: String?,
@@ -43,14 +45,11 @@ final class AuthManager {
                 Auth.auth().currentUser?.sendEmailVerification()
                 
                 guard error == nil else {
-                    print("Account created has been failed")
                     if error?.localizedDescription == "The email address is already in use by another account." {
                         currentViewController?.showAlert(title: "Wrong Email", message: "Email has been already used")
                     }
                     return
                 }
-                
-                print("A new account was created successfuly")
                 currentViewController?.showAlert(title: "Account has been created",
                                          message: "Link to the account verification has been sent to your email address") {
                     currentViewController?.navigationController?.popViewController(animated: true)
@@ -59,12 +58,14 @@ final class AuthManager {
         }
     }
     
-    /// This is a static function that allows a user to sign in to the app using their email and password. It takes in three parameters: the user's email address, the user's password, and the view controller that is calling the function. If the user's email and password are correct, and the user's email address is verified, the function segues to the Lock Screen view controller. Otherwise, it shows an alert with an error message.
-    ///
-    /// - Parameters:
-    ///   - userEmail: A String that represents the user's email address. This parameter is optional and defaults to an empty string if it is not provided.
-    ///   - userPassword: A String that represents the user's password. This parameter is optional and defaults to an empty string if it is not provided.
-    ///   - currentViewController: A UIViewController that represents the view controller that is calling the function.
+    /**
+     This is a static function that allows a user to sign in to the app using their email and password. It takes in three parameters: the user's email address, the user's password, and the view controller that is calling the function. If the user's email and password are correct, and the user's email address is verified, the function segues to the Lock Screen view controller. Otherwise, it shows an alert with an error message.
+    
+     - Parameters:
+       - userEmail: A String that represents the user's email address. This parameter is optional and defaults to an empty string if it is not provided.
+       - userPassword: A String that represents the user's password. This parameter is optional and defaults to an empty string if it is not provided.
+       - currentViewController: A UIViewController that represents the view controller that is calling the function.
+     */
     static func signIn(_ userEmail: String?,
                        _ userPassword: String?,
                        _ currentViewController: UIViewController) {
@@ -78,20 +79,16 @@ final class AuthManager {
             guard error == nil else {
                 currentViewController?.showAlert(title: "Account hasn't been found",
                                 message: "Please check your email and password")
-                print("Wrong login or password")
                 return
             }
             
             if Auth.auth().currentUser?.isEmailVerified == true {
-                print("Welcome to the app")
-                print("User is verified")
                 
                 let storyboard = UIStoryboard(name: "LockScreenViewController", bundle: nil)
                 let viewController = storyboard.instantiateViewController(withIdentifier: "LockScreenViewController") as! LockScreenViewController
                 currentViewController?.navigationController?.setViewControllers([viewController], animated: true)
                 
             } else {
-                print("User still need to verify email")
                 currentViewController?.showAlert(title: "Account is not verified",
                                 message: "Please check your email address for verification")
             }
@@ -112,12 +109,13 @@ final class AuthManager {
         }
     }
     
+    /**
+     This is a static function that resets the password for a user's email address. It takes in two parameters: the user's email address and the view controller that is calling the function. If the email address is valid, the function sends a password reset email to the user's email address.
     
-    /// This is a static function that resets the password for a user's email address. It takes in two parameters: the user's email address and the view controller that is calling the function. If the email address is valid, the function sends a password reset email to the user's email address.
-    ///
-    /// - Parameters:
-    ///   - userEmail: A String value that represents the user's email address.
-    ///   - viewController: A UIViewController that represents the view controller that is calling the function
+     - Parameters:
+       - userEmail: A String value that represents the user's email address.
+       - viewController: A UIViewController that represents the view controller that is calling the function
+     */
     static func resetPassword(_ userEmail: String?, _ viewController: UIViewController) {
             let email = userEmail ?? ""
             
